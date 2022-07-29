a2f3c931fa9d0ae12dec3260d9e46d5f9044bc31
Yuliya Kurilchik <hidrainn@gmail.com>
2022-07-30 00:49:45 +0200

    Created a Categories component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,34 @@
+import { useState } from 'react'
+
 export function Categories() {
+
+    const categories = [
+        'Все',
+        'Мясные',
+        'Вегетарианские',
+        'Гриль',
+        'Острые',
+        'Закрытые'
+    ]
+
+    const [activeIndex, setActiveIndex] = useState(0)
+
+    const onClickCategory = (index) => {
+        setActiveIndex(index)
+    }
+
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {
+                    categories.map((value, index) => (
+                        <li onClick={() => (onClickCategory(index))}
+                            className={activeIndex === index ? "active" : ''} >
+                            {value}
+                        </li>
+                    ))
+                }
             </ul>
-        </div>
+        </div >
     )
 }
